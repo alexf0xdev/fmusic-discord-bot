55998c05eab4809a002258aa3c47cf0cedd834fc
alexf0xdev <glebf78@gmail.com>
2025-05-20 16:50:59 +0500

    update format util

--- src/bot/utils/format.util.ts
+++ src/bot/utils/format.util.ts
@@ -1,9 +1,9 @@
 export const formatMilliseconds = (milliseconds: number) => {
   const minutes = Math.floor((milliseconds / (60 * 1000)) % 60);
-  const secs = Math.floor((milliseconds / 1000) % 60);
+  const seconds = Math.floor((milliseconds / 1000) % 60);
 
   return [
     minutes.toString().padStart(2, '0'),
-    secs.toString().padStart(2, '0'),
+    seconds.toString().padStart(2, '0'),
   ].join(':');
 };
