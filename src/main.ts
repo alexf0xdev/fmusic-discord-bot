846ee9f5196d2795330b5ca0511fa1f84b1a3c4d
alexf0xdev <glebf78@gmail.com>
2025-05-24 19:49:39 +0500

    move sentry init to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
-import './instrument';
-
 import { NestFactory } from '@nestjs/core';
+import Sentry from '@sentry/nestjs';
+import { nodeProfilingIntegration } from '@sentry/profiling-node';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(AppModule);
+
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [nodeProfilingIntegration()],
+    // Tracing must be enabled for profiling to work
+    tracesSampleRate: 1.0,
+    // Set sampling rate for profiling - this is evaluated only once per SDK.init call
+    profileSessionSampleRate: 1.0,
+    // Trace lifecycle automatically enables profiling during active traces
+    profileLifecycle: 'trace',
+
+    // Setting this option to true will send default PII data to Sentry.
+    // For example, automatic IP address collection on events
+    sendDefaultPii: true,
+  });
 
   await app.init();
 }
