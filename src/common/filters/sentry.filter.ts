e36eac2ebcb3f719d65bef531ffadffc72b95f28
alexf0xdev <glebf78@gmail.com>
2025-06-04 19:31:45 +0500

    update sentry filter

--- src/common/filters/sentry.filter.ts
+++ src/common/filters/sentry.filter.ts
@@ -1,19 +1,11 @@
 import { type ArgumentsHost, Catch } from '@nestjs/common';
 import { captureException } from '@sentry/nestjs';
 import { SentryGlobalFilter } from '@sentry/nestjs/setup';
-import { NecordArgumentsHost } from 'necord';
 
 @Catch()
 export class SentryFilter extends SentryGlobalFilter {
   public catch(exception: unknown, host: ArgumentsHost) {
-    const necordArgumentsHost = NecordArgumentsHost.create(host);
-    const type = necordArgumentsHost.getType();
-
-    if (type !== 'necord') {
-      return super.catch(exception, host);
-    }
-
     captureException(exception);
-    throw exception;
+    return super.catch(exception, host);
   }
 }
