1fbd064d7bd6385f67bd20ad6b6b92b7d8514d13
alexf0xdev <121234531+alexf0xdev@users.noreply.github.com>
2025-09-28 11:39:05 +0500

    fix discord shards

--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -24,6 +24,7 @@ import Commands from './commands';
         presence: {
           activities: [{ name: '/help', type: ActivityType.Listening }],
         },
+        shards: 'auto'
       }),
     }),
     NecordLavalinkModule.forRootAsync({
